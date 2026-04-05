da5d61bfc977a31d054c06dec8885bee0927b59d
misaelgarcia113 <mmg2031181034@upatlacomulco.edu.mx>
2026-04-05 17:27:30 -0600

    fix static files

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
+// Servir archivos estáticos directamente
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+$file = __DIR__ . $uri;
+if ($uri !== '/' && file_exists($file) && !is_dir($file)) {
+    return false;
+}
 
 use CodeIgniter\Boot;
 use Config\Paths;
