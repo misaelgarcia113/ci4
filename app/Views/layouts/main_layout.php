e1944ee21cc0268a110e7550cb014dd65e41c42d
misaelgarcia113 <mmg2031181034@upatlacomulco.edu.mx>
2026-04-05 17:52:57 -0600

    fix css js paths

--- app/Views/layouts/main_layout.php
+++ app/Views/layouts/main_layout.php
@@ -5,18 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Main CSS-->                                 
-    <link rel="stylesheet" type="text/css" href="<?= base_url('/public/css/main.css'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('/css/main.css'); ?>">
     <!-- Font-icon css-->
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <title>Login</title>
   </head>
 <body>
-
+ 
     <?= $this->renderSection('content') ?>
-
-    <script src="<?= base_url('/public/js/jquery-3.7.0.min.js'); ?>"></script>
-    <script src="<?= base_url('/public/js/bootstrap.min.js');    ?>"></script>
-    <script src="<?= base_url('/public/js/main.js');             ?>"></script>
+ 
+    <script src="<?= base_url('/js/jquery-3.7.0.min.js'); ?>"></script>
+    <script src="<?= base_url('/js/bootstrap.min.js');    ?>"></script>
+    <script src="<?= base_url('/js/main.js');             ?>"></script>
     <script type="text/javascript">
       // Login Page Flipbox control
       $('.login-content [data-toggle="flip"]').click(function() 
@@ -27,4 +27,3 @@
     </script>
   </body>
 </html>
-  
