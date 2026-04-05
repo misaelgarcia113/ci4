46c23199f7e7b06814777b99ae9d6a34c1dc054b
misaelgarcia113 <mmg2031181034@upatlacomulco.edu.mx>
2026-04-05 17:17:35 -0600

    fix baseURL App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/ci4';
+    public string $baseURL = 'https://ci4-production-dc95.up.railway.app/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
