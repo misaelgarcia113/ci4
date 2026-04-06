460a7ce947e2f06615a85f385449959e7a8d1a1c
misaelgarcia113 <mmg2031181034@upatlacomulco.edu.mx>
2026-04-05 21:27:53 -0600

    fix css paths

--- app/Views/admin/dashboard.php
+++ app/Views/admin/dashboard.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="<?= base_url('/public/css/main.css'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('/css/main.css'); ?>">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/2.3.7/css/dataTables.dataTables.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
